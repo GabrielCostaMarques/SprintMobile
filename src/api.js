aae781ea3e57f5eb39d1021791ba3c12bf0fe0c5
Matheus <email@email.com>
2023-10-01 14:43:33 -0300

    feat:endereco local automatico

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
+import localhost from 'react-native-localhost';
 
-export const API_URL = 'http://localhost:8080/';
+export const API_URL = `http://${localhost}:8383/`;
 
 export const api = axios.create({
   baseURL: API_URL,
