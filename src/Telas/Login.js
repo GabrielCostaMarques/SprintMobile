99297332d30a5998646ec9b9af76519f1b6e739a
Matheus <email@email.com>
2023-10-14 18:25:49 -0300

    wip:modal esqueci a senha

--- src/Telas/Login.js
+++ src/Telas/Login.js
@@ -7,30 +7,45 @@ import {
   Image,
   StatusBar,
   StyleSheet,
+  Modal,
 } from "react-native";
 import { Contexto } from "../components/contexto";
 import { api, API_URL } from "../api";
 import { onSucess, onError } from "../components/Toast";
 
 const Login = ({ navigation }) => {
-  const contexto = useContext(Contexto);
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
+  const [isModalVisible, setModalVisible] = useState(false);
 
   const handleLogin = async () => {
     try {
-     let resp =  await api.post(`${API_URL}usuarios/login`, {
+      let resp = await api.post(`${API_URL}usuarios/login`, {
         email,
         senha,
       });
-      contexto.id = resp.data.id
+      contexto.id = resp.data.id;
       onSucess(`Login com Sucesso!`);
       navigation.navigate("‎");
-      
     } catch (error) {
-      onError(`${error.response.data}` ?? `Falha no login. Verifique suas credenciais!!`);
+      onError(error.response.data ?? `Falha no login. Verifique suas credenciais!!`);
       console.log(JSON.stringify(error.message));
     }
+  };
+
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
+
+  const handleSendEmail = () => {
+    // todo enviar o email
+    // console.log(`Enviando email para: ${email}`);
+    onSucess(`Email enviado com Sucesso!`);
+
+    // limpa o campo do email do modal apos enviar o email
+    setEmail("")
+    // fecha o modal
+    toggleModal();
   };
 
   return (
@@ -55,13 +70,15 @@ const Login = ({ navigation }) => {
           minLength={1}
         />
       </View>
-
+      <TouchableOpacity onPress={toggleModal}>
+        <View>
+          <Text style={style.esqueceuSenhaText}>Esqueceu a Senha?</Text>
+        </View>
+      </TouchableOpacity>
       <View>
         <TouchableOpacity
           onPress={() => {
             handleLogin();
-            // contexto.salvar
-            // navigation.navigate('Menu')
           }}
         >
           <View style={style.btnLogin}>
@@ -69,6 +86,31 @@ const Login = ({ navigation }) => {
           </View>
         </TouchableOpacity>
       </View>
+
+      <Modal visible={isModalVisible} animationType="slide">
+        <View style={style.modalContainer}>
+          <View style={style.modalContent}>
+            {/* <Text style={style.modalText}>Esqueceu sua senha?</Text> */}
+            <Text style={style.modalText}>
+              Insira seu email e enviaremos instruções para redefinir sua senha.
+            </Text>
+            <TextInput
+              style={style.input}
+              placeholder="Email"
+              value={email}
+              onChangeText={(text) => setEmail(text)}
+            />
+            
+            <TouchableOpacity  style={style.modalButtonEnviar} onPress={handleSendEmail}>
+              <Text style={style.modalButtonText}>Enviar</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={style.modalButtonCancelar} onPress={toggleModal}>
+              <Text style={style.modalButtonText}>Cancelar</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 };
@@ -105,6 +147,7 @@ const style = StyleSheet.create({
     paddingVertical: 20,
     paddingHorizontal: 20,
     color: "black",
+    borderWidth:0.5
   },
 
   btnLogin: {
@@ -121,5 +164,50 @@ const style = StyleSheet.create({
   btnText: {
     fontSize: 30,
     color: "white",
+  },
+
+  esqueceuSenhaText: {
+    textDecorationStyle: "solid",
+    textDecorationLine: "underline",
+    marginVertical: 10,
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "white",
+  },
+
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalContent: {
+    width: 300,
+    padding: 20,
+    backgroundColor: "white",
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  modalText: {
+    fontSize: 18,
+    marginBottom: 10,
+    color: "black",
+  },
+  modalButtonEnviar: {
+    backgroundColor: "green",
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 5,
+    margin: 5,
+  },
+  modalButtonCancelar: {
+    backgroundColor: "red",
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 5,
+    margin: 5,
+  },
+  modalButtonText: {
+    color: "white",
+    fontSize: 16,
   },
 });
